a5458ffaf7d519c8bce4b9494af0cffbb6fb08c0
kkm <kubasurdej@gmail.com>
2020-11-15 00:10:04 +0100

    Add typing to getting heroes, implement getHeroesCount method

--- src/services/hero.service.ts
+++ src/services/hero.service.ts
@@ -5,16 +5,23 @@ const BASE_HEROES_PATH = "people/";
 
 export const getHero = async (id: string | number) => {
   const resourcePath = `${BASE_HEROES_PATH}/${id}`;
-  const heroData = await callStartwarsApi(resourcePath);
+  const heroData = await callStartwarsApi<HeroResponse>(resourcePath);
 
   return heroData;
 };
 
 export const getHeroesPage = async (pageNumber: number | string = 1) => {
   const resourcePath = `${BASE_HEROES_PATH}/?page=${pageNumber}`;
-  const heroesPage = await callStartwarsApi(resourcePath);
+  const heroesPage = await callStartwarsApi<HeroPageResponse>(resourcePath);
 
   return heroesPage;
+};
+
+export const getHeroesCount = async (): Promise<number> => {
+  const heroesPage = await getHeroesPage();
+  const numberOfHeroes = heroesPage.count;
+
+  return numberOfHeroes;
 };
 
 export interface HeroResponse {
