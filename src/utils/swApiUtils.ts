49511d34b2c0be4a41b24dd727466fa569cf4a75
kkm <kubasurdej@gmail.com>
2020-11-15 00:56:37 +0100

    Add util for getting id from uri

--- src/utils/swApiUtils.ts
+++ src/utils/swApiUtils.ts
@@ -1,3 +1,18 @@
+import { notEmptyString } from "@/utils/arrayUtils";
+import { GraphQLError } from "graphql";
+
+export const getIdFromUri = (uri: string) => {
+  let id: number;
+  try {
+    id = Number(uri.split("/").filter(notEmptyString).slice(-1)[0]);
+  } catch (e) {
+    console.error(`An error occured when trying to get id from uri: ${e}`);
+    throw new GraphQLError("An unknown error occured");
+  }
+
+  return id;
+};
+
 export type PaginationResponse<T> = {
   count: number;
   next: string | null;
