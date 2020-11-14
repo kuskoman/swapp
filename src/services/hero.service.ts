2870a3053a21c9b0a3909e053a189d619ec1bdb5
kkm <kubasurdej@gmail.com>
2020-11-14 23:33:55 +0100

    Add hero schema, start creating hero service

--- src/services/hero.service.ts
+++ src/services/hero.service.ts
@@ -1,1 +1,19 @@
+import { callStartwarsApi } from "./starWars.service";
+
+const BASE_HEROES_PATH = "people/";
+
+export const getHero = async (id: string | number) => {
+  const resourcePath = `${BASE_HEROES_PATH}/${id}`;
+  const heroData = await callStartwarsApi(resourcePath);
+
+  return heroData;
+};
+
+export const getHeroesPage = async (pageNumber: number | string = 1) => {
+  const resourcePath = `${BASE_HEROES_PATH}/?page=${pageNumber}`;
+  const heroesPage = await callStartwarsApi(resourcePath);
+
+  return heroesPage;
+};
+
 export type Gender = "male" | "female" | "n/a" | string; // this makes sense only for autocompletion
