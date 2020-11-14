9a26f6560f52902690ef587941f04ee20e11954b
kkm <kubasurdej@gmail.com>
2020-11-14 21:04:06 +0100

    Handle errors when calling starwars api

---
+++ src/services/hero.service.ts
@@ -0,0 +1,1 @@
+export type Gender = "male" | "female" | "n/a" | string; // this makes sense only for autocompletion
--- src/services/starWars.service.ts
+++ src/services/starWars.service.ts
@@ -1,11 +1,26 @@
 import axios, { AxiosResponse } from "axios";
+import { GraphQLError } from "graphql";
 
 export const SWAPI_BASE_URL = "https://swapi.dev/api/";
 
 export const callStartwarsApi = async <T>(
-  url: string
+  resource: string
 ): Promise<AxiosResponse<T>> => {
-  const response = await axios.get<T>(`https://swapi.dev/api/${url}`);
+  let response: AxiosResponse<T>;
+
+  try {
+    response = await axios.get<T>(`${SWAPI_BASE_URL}${resource}`);
+  } catch (e) {
+    throw new ExternalServiceError();
+  }
 
   return response;
 };
+
+export class ExternalServiceError extends GraphQLError {
+  constructor() {
+    super("Error of external service");
+    this.name = "ExternalServiceError";
+    Object.setPrototypeOf(this, ExternalServiceError.prototype);
+  }
+}
