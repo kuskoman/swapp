7a18d81805fddd3a4d7dbfdeb98ecf51e9fe13dd
kkm <kubasurdej@gmail.com>
2020-11-14 18:36:57 +0100

    Create migration adding users

---
+++ src/migrations/1605373078669-createUser.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class createUser1605373078669 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "serial",
+            isPrimary: true,
+          },
+          {
+            name: "email",
+            type: "varchar",
+          },
+          {
+            name: "password_digest",
+            type: "varchar",
+          },
+          {
+            name: "hero_id",
+            type: "varchar",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.dropTable("users");
+  }
+}
