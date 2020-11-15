ad5f9bb1fab381c66c29b1ab75e5e53631b33a99
kkm <kubasurdej@gmail.com>
2020-11-15 16:26:16 +0100

    Replace any type with valid type

--- src/auth/authorizeApiRequest.ts
+++ src/auth/authorizeApiRequest.ts
@@ -2,10 +2,13 @@ import { UserID } from "@/types";
 import {
   extractBearerToken,
   getAuthorizationToken,
+  RequestLikeWithAuthHeaders,
 } from "@/utils/requestUtils";
 import { getUserIdFromToken } from "./sessions";
 
-export const getUserId = async (request: any): Promise<UserID> => {
+export const getUserId = async (
+  request: RequestLikeWithAuthHeaders
+): Promise<UserID> => {
   const token = getAuthorizationToken(request);
   const extractedToken = extractBearerToken(token);
   const userId = await getUserIdFromToken(extractedToken);
