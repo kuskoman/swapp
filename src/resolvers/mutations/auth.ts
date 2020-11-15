86f3c794c45b2903d4140ed8c6e9bc039b4c7a7b
kkm <kubasurdej@gmail.com>
2020-11-15 01:12:55 +0100

    Create auth mutations

--- src/resolvers/mutations/auth.ts
+++ src/resolvers/mutations/auth.ts
@@ -1,5 +1,20 @@
+import { generateToken } from "@/auth/sessions";
 import { MutationResolvers } from "@/generated/graphql";
+import { authorizeUser, createUser } from "@/services/user.service";
 
-const auth: MutationResolvers = {};
+const auth: MutationResolvers = {
+  signup: async (_parent, { input }, _ctx) => {
+    const user = await createUser(input);
+    const token = await generateToken(user.id);
+
+    return { user, token };
+  },
+  login: async (_parent, { input }, _ctx) => {
+    const user = await authorizeUser(input);
+    const token = await generateToken(user.id);
+
+    return { user, token };
+  },
+};
 
 export default auth;
