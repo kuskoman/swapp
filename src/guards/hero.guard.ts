5367eb8c6ddd1931e7e423e79c20b28e5a20387f
kkm <kubasurdej@gmail.com>
2020-11-15 15:26:10 +0100

    Refactory film guard, create hero guard

--- src/guards/hero.guard.ts
+++ src/guards/hero.guard.ts
@@ -1,13 +1,10 @@
 import { User } from "@/entities/user.entity";
-import { getHeroByUri } from "@/services/hero.service";
 import { UserID } from "@/types";
 import { convertToUser, ForbiddenError } from "@/utils/guardUtils";
 
-export const authorizeFilm = async (uri: string, user: UserID | User) => {
+export const authorizeHero = async (uri: string, user: UserID | User) => {
   user = await convertToUser(user);
-
-  const hero = await getHeroByUri(user.hero_uri);
-  const authorized = hero.films.includes(uri);
+  const authorized = user.hero_uri === uri;
 
   if (!authorized) {
     throw new ForbiddenError();
--- src/utils/guardUtils.ts
+++ src/utils/guardUtils.ts
@@ -1,4 +1,18 @@
+import { User } from "@/entities/user.entity";
+import { getUser } from "@/services/user.service";
+import { UserID } from "@/types";
 import { GraphQLError } from "graphql";
+
+export const convertToUser = async (
+  userInput: UserID | User
+): Promise<User> => {
+  if (typeof userInput !== "object") {
+    const user = await getUser(userInput);
+    return user as User;
+  }
+
+  return userInput;
+};
 
 export class ForbiddenError extends GraphQLError {
   constructor() {
