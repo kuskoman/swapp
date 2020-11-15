d59ba631744e94e12dea9231d70c064e506e0b52
kkm <kubasurdej@gmail.com>
2020-11-15 01:28:36 +0100

    Add method for getting film by uri

--- src/services/films.service.ts
+++ src/services/films.service.ts
@@ -1,12 +1,18 @@
-import { callStartwarsApi } from "./starWars.service";
+import { callStartwarsApi, rawCallStarwarsApi } from "./starWars.service";
 
 export const BASE_FILMS_PATH = "films/";
 
-export const getFilm = async (id: string | number) => {
+export const getFilmById = async (
+  id: string | number
+): Promise<FilmResponse> => {
   const resourcePath = `${BASE_FILMS_PATH}/${id}`;
   const heroData = await callStartwarsApi<FilmResponse>(resourcePath);
 
   return heroData;
+};
+
+export const getFilmByUri = async (uri: string): Promise<FilmResponse> => {
+  return rawCallStarwarsApi(uri);
 };
 
 export interface FilmResponse {
