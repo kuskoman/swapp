a1693861ecad2ed61704be326b2f992233e50bbe
kkm <kubasurdej@gmail.com>
2020-11-15 01:05:09 +0100

    Add context to server

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,21 @@
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
+import { getUserId } from "./auth/authorizeApiRequest";
 import schema from "./schema";
+import { getUser } from "./services/user.service";
 
 const server = express();
-server.use(
-  "/graphql",
-  graphqlHTTP({
+server.use("/graphql", (req) => {
+  return graphqlHTTP({
     schema,
     graphiql: process.env.NODE_ENV === "development",
-  })
-);
+    context: {
+      currentUser: async () => {
+        const userId = await getUserId(req);
+        return getUser(userId);
+      },
+    },
+  });
+});
 
 export default server;
